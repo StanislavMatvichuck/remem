703b52ee53c2e90de21b21525bb9b6b5b1e51446
StanislavMck2120 <stanislavmat@rozdoum.com>
2022-04-21 13:27:47 +0300

    SettingsController code formatting

--- Remem/Controllers/Settings/SettingsController.swift
+++ Remem/Controllers/Settings/SettingsController.swift
@@ -8,53 +8,26 @@
 import UIKit
 
 class SettingsController: UIViewController {
-    //
-    
-    // MARK: - Private properties
-    
-    //
-    
-    fileprivate let viewRoot = SettingsView()
-    
-    //
-    
-    // MARK: - Initialization
-    
-    //
-    
-    init() {
-        super.init(nibName: nil, bundle: nil)
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    //
-    
+    // MARK: - Init
+    init() { super.init(nibName: nil, bundle: nil) }
+    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+
     // MARK: - View lifecycle
-    
-    //
-    
+    fileprivate let viewRoot = SettingsView()
     override func loadView() { view = viewRoot }
-    
     override func viewDidLoad() {
         title = "Settings"
-        
         setupEventsHandlers()
     }
-    
-    //
+}
 
-    // MARK: - Events handling
-
-    //
-    
+// MARK: - User input
+extension SettingsController {
     private func setupEventsHandlers() {
         viewRoot.onboardingButton.addGestureRecognizer(
             UITapGestureRecognizer(target: self, action: #selector(handlePressOnboarding)))
     }
-    
+
     @objc func handlePressOnboarding() {
         guard let main = presentingViewController as? EntriesListController else { return }
         dismiss(animated: true) { main.startOnboarding() }
