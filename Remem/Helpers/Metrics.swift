57b1396bd8abb521cdb762f828e69ead4d69e645
StanislavMck2120 <stanislavmat@rozdoum.com>
2022-04-28 21:00:43 +0300

    Metrics refactoring

--- Remem/Helpers/Metrics.swift
+++ Remem/Helpers/Metrics.swift
@@ -9,74 +9,40 @@ import UIKit
 
 extension CGFloat {
     // MARK: Relative unit definition
+    static var rem: CGFloat = {
+        let width: CGFloat = UIScreen.main.bounds.width
 
-    static var rem: CGFloat {
-        let screen: CGRect = UIScreen.main.bounds
-
-        if screen.width >= 768 {
+        if width >= 768 {
             return 23.0 // ipad
-        } else if screen.width >= 414 {
+        } else if width >= 414 {
             return 18.0 // 8+, XR, XR max
-        } else if screen.width >= 375 {
+        } else if width >= 375 {
             return 15.0 // 6, 6+, 8
-        } else if screen.width >= 320 {
+        } else if width >= 320 {
             return 14.0 // SE, 5
         }
 
         return 12.0 // not used
-    }
+    }()
 
     // MARK: Main metrics
-
-    static var xs: CGFloat { return rem / 2 }
-    static var sm: CGFloat { return rem }
-    static var md: CGFloat { return 1.8 * rem }
-    static var lg: CGFloat { return 3 * rem }
-
+    static var xs: CGFloat { rem / 2 }
+    static var sm: CGFloat { rem }
+    static var md: CGFloat { 1.8 * rem }
+    static var lg: CGFloat { 3 * rem }
     // MARK: Screen size
-
-    static var wScreen: CGFloat { return UIScreen.main.bounds.width }
-    static var hScreen: CGFloat { return UIScreen.main.bounds.height }
-
+    static var wScreen: CGFloat { UIScreen.main.bounds.width }
+    static var hScreen: CGFloat { UIScreen.main.bounds.height }
     // MARK: Fonts
-
-    static var font1: CGFloat { return UIFont.labelFontSize }
-    static var font2: CGFloat { return 2 * .font1 }
-
+    static var font1: CGFloat { UIFont.labelFontSize }
+    static var font2: CGFloat { 2 * .font1 }
     // MARK: Extras
-
     static var hairline: CGFloat { return 1.0 / UIScreen.main.scale }
-//    static var hInput: CGFloat { return 3 * rem }
-//    static var hButton: CGFloat { return 3.2 * rem }
-    //
 
-    // MARK: Main screen layout
-
-    //
-
-    static var r1: CGFloat {
-        return 2 * rem
-    }
-
-    static var r2: CGFloat {
-        return r1 + .delta1
-    }
-
-    static var delta1: CGFloat {
-        return .xs
-    }
-
-    static var d1: CGFloat {
-        return 2 * r1
-    }
-
-    static var d2: CGFloat {
-        return 2 * r2
-    }
-
-    //
-
-    // MARK: - Points list layout
-
-    //
+    // MARK: - Remem specific layout
+    static var r1: CGFloat { 2 * rem }
+    static var r2: CGFloat { r1 + .delta1 }
+    static var delta1: CGFloat { .xs }
+    static var d1: CGFloat { 2 * r1 }
+    static var d2: CGFloat { 2 * r2 }
 }
