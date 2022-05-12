24b7ee15085f7ceb53c6af3119c46fe0b19e3f65
StanislavMck2120 <stanislavmat@rozdoum.com>
2022-05-12 16:33:31 +0300

    clock different stitch variants drawing

--- Remem/Controllers/EntryDetails/Clock.swift
+++ Remem/Controllers/EntryDetails/Clock.swift
@@ -8,11 +8,25 @@
 import UIKit
 
 class Clock: UIView {
+    enum StitchVariant: Int {
+        case empty
+        case little
+        case mid
+        case big
+    }
+
     // MARK: - Properties
-    let lineWidth = 32.0
+    let maximumStitchHeight = 32.0
     var circleMarginFromBounds: CGFloat = 0.0
-    var label15VerticalConstraint: NSLayoutConstraint!
     var leastDimension: CGFloat { min(bounds.width, bounds.height) }
+
+    var stitchVariantsArray: [StitchVariant] = {
+        var array: [StitchVariant] = []
+        for i in 0 ... 71 {
+            array.append(StitchVariant(rawValue: Int.random(in: 0 ... 3))!)
+        }
+        return array
+    }()
 
     // MARK: - Init
     override init(frame: CGRect) {
@@ -27,41 +41,61 @@ class Clock: UIView {
     // MARK: - Drawing
     override func draw(_ rect: CGRect) {
         guard let context = UIGraphicsGetCurrentContext() else { return }
-        drawStitches(context)
+        placeStitchesAround(in: context)
     }
 
-    private func drawStitches(_ context: CGContext) {
-        let center = CGPoint(x: bounds.midX, y: bounds.midY)
-        let radius = ((leastDimension - lineWidth / 2) / 2.0) - circleMarginFromBounds
+    private func placeStitchesAround(in context: CGContext) {
         let toothAngle: CGFloat = 5
-        let stitchWidth = 2.0
-        let stitchHeight = lineWidth * 0.5
 
-        let stitch = CGRect(x: center.x - stitchWidth,
-                            y: center.y - radius - stitchHeight / 2,
+        for (index, angle) in stride(from: toothAngle, through: 360, by: toothAngle).enumerated() {
+            let stitchVariant = stitchVariantsArray[index]
+            let path = makeStitchPath(for: stitchVariant)
+
+            path.apply(CGAffineTransform(translationX: -center.x, y: -center.y))
+            path.apply(CGAffineTransform(rotationAngle: angle * .pi / 180))
+            path.apply(CGAffineTransform(translationX: center.x, y: center.y))
+
+            context.addPath(path.cgPath)
+            context.setFillColor(stitchBackgroundColor(for: stitchVariant))
+            context.fillPath(using: .winding)
+        }
+    }
+
+    private func makeStitchPath(for variant: StitchVariant) -> UIBezierPath {
+        let radius = (leastDimension / 2.0) - circleMarginFromBounds - maximumStitchHeight
+        let stitchWidth = 4.0
+        let stitchHeight = stitchHeight(for: variant)
+
+        let stitch = CGRect(x: center.x - stitchWidth / 2,
+                            y: center.y - stitchHeight - radius,
                             width: stitchWidth,
                             height: stitchHeight)
 
-        let stitchPath = UIBezierPath(roundedRect: stitch, cornerRadius: stitchWidth / 2)
-        context.setFillColor(UIColor.blue.cgColor)
+        return UIBezierPath(roundedRect: stitch, cornerRadius: stitchWidth / 2)
+    }
 
-        for _ in stride(from: toothAngle, through: 360, by: toothAngle) {
-            // Move origin to center of the circle
-            stitchPath.apply(CGAffineTransform(translationX: -center.x, y: -center.y))
-            // Rotate
-            stitchPath.apply(CGAffineTransform(rotationAngle: toothAngle * .pi / 180))
-            // Move origin back to original location
-            stitchPath.apply(CGAffineTransform(translationX: center.x, y: center.y))
+    private func stitchBackgroundColor(for variant: StitchVariant) -> CGColor {
+        switch variant {
+        case .empty: return UIColor.secondarySystemBackground.cgColor
+        case .little: return UIColor.blue.cgColor
+        case .mid: return UIColor.blue.cgColor
+        case .big: return UIColor.blue.cgColor
+        }
+    }
 
-            context.addPath(stitchPath.cgPath)
-            context.fillPath(using: .winding)
+    private func stitchHeight(for variant: StitchVariant) -> CGFloat {
+        switch variant {
+        case .empty: return maximumStitchHeight * 0.33
+        case .little: return maximumStitchHeight * 0.33
+        case .mid: return maximumStitchHeight * 0.66
+        case .big: return maximumStitchHeight
         }
     }
 
     override func layoutSubviews() {
         NSLayoutConstraint.activate([
-            subviews[1].centerXAnchor.constraint(equalTo: centerXAnchor, constant: (leastDimension - lineWidth / 2) / 2),
-            subviews[3].centerXAnchor.constraint(equalTo: centerXAnchor, constant: (leastDimension - lineWidth / 2) / -2),
+            subviews[1].centerXAnchor.constraint(equalTo: centerXAnchor, constant: (leastDimension - maximumStitchHeight / 2) / 2),
+            subviews[3].centerXAnchor.constraint(equalTo: centerXAnchor, constant: (leastDimension - maximumStitchHeight / 2) / -2),
         ])
 
         super.layoutSubviews()
