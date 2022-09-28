42e2aa200bdbadcc7424fba902933e46b8e0a6f6
StanislavMck2120 <stanislavmat@rozdoum.com>
2022-09-29 01:28:05 +0300

    small cleanup

--- Application/Delegates/AppDelegate.swift
+++ Application/Delegates/AppDelegate.swift
@@ -10,68 +10,18 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        UNUserNotificationCenter.current().delegate = self
-        /// this call is made to avoid application reinstall
-        registerCustomActions()
-        return true
-    }
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions:
+                     [UIApplication.LaunchOptionsKey: Any]?) ->
+        Bool
+    { return true }
 
     // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
-    // MARK: - Local notifications
-    private func registerCustomActions() {
-        let add = UNNotificationAction(
-            identifier: ActionIdentifier.add.rawValue,
-            title: NSLocalizedString("notification.action.add", comment: "Notification actions"))
-
-        let category = UNNotificationCategory(
-            identifier: happeningsManipulcationNotificationsCategory,
-            actions: [add],
-            intentIdentifiers: [])
-
-        UNUserNotificationCenter.current().setNotificationCategories([category])
-    }
-}
-
-// MARK: - Notifications actions handling
-extension AppDelegate: UNUserNotificationCenterDelegate {
-    func userNotificationCenter(
-        _ center: UNUserNotificationCenter,
-        willPresent notification: UNNotification,
-        withCompletionHandler completionHandler:
-        @escaping (UNNotificationPresentationOptions) -> Void)
+    func application(_ application: UIApplication,
+                     configurationForConnecting connectingSceneSession: UISceneSession,
+                     options: UIScene.ConnectionOptions) ->
+        UISceneConfiguration
     {
-        completionHandler([.badge, .sound, .banner])
-    }
-
-    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse) async {
-        let identity = response.notification.request.content.categoryIdentifier
-        guard
-            identity == happeningsManipulcationNotificationsCategory,
-            let action = ActionIdentifier(rawValue: response.actionIdentifier),
-            action == .add,
-            let eventIdString = response.notification.request.content.userInfo["identifier"] as? String
-        else { return }
-
-        addHappening(eventId: eventIdString)
-    }
-
-    private func addHappening(eventId: String) {
-//        let service = EventsListService(EventsRepository())
-//        service.makeHappening(by: eventId)
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 }
