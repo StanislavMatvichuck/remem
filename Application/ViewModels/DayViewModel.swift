4e8d0e871d5c88b5b2bf2c12f115d1afd1191a5e
StanislavMck2120 <stanislavmat@rozdoum.com>
2022-09-29 13:32:09 +0300

    Current time instead of 23:59 at day controller

--- Application/ViewModels/DayViewModel.swift
+++ Application/ViewModels/DayViewModel.swift
@@ -5,8 +5,8 @@
 //  Created by Stanislav Matvichuck on 06.08.2022.
 //
 
-import Foundation
 import Domain
+import Foundation
 import IosUseCases
 
 public protocol DayViewModeling:
@@ -35,7 +35,17 @@ public class DayViewModel: DayViewModeling {
     private var shownHappenings: [Happening] { event.happenings(forDay: date) }
     // MARK: - Init
     public init(date: Date, event: Event, editUseCase: EventEditUseCasing) {
-        self.date = date
+        func updateTimeToCurrent(forDate: Date) -> Date {
+            let c = Calendar.current
+            let currentDate = Date.now
+            let componentsWithCurrentTime = c.dateComponents([.year, .month, .day, .hour, .minute, .second], from: currentDate)
+            var componentsToBeChanged = c.dateComponents([.year, .month, .day, .hour, .minute, .second], from: forDate)
+            componentsToBeChanged.hour = componentsWithCurrentTime.hour
+            componentsToBeChanged.minute = componentsWithCurrentTime.minute
+            return c.date(from: componentsToBeChanged)!
+        }
+
+        self.date = updateTimeToCurrent(forDate: date)
         self.event = event
         self.editUseCase = editUseCase
     }
