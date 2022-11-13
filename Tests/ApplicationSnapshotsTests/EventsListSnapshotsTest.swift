7b9f9a9495a63a8e263467db48747e608309042b
StanislavMck2120 <stanislavmat@rozdoum.com>
2022-11-13 22:06:32 +0200

    EventsListSnapshotsTests navigation controller

--- Tests/ApplicationSnapshotsTests/EventsListSnapshotsTest.swift
+++ Tests/ApplicationSnapshotsTests/EventsListSnapshotsTest.swift
@@ -9,42 +9,42 @@
 import iOSSnapshotTestCase
 
 class EventsListSnapshotsTest: FBSnapshotTestCase {
+    var sut: EventsListController!
+
     override func setUp() {
         super.setUp()
-
         recordMode = true
+
+        let viewModel = EventsListViewModelFake()
+        let view = EventsListView()
+
+        sut = EventsListController(
+            viewRoot: view,
+            viewModel: viewModel
+        )
+
+        let nav = ApplicationFactory.makeStyledNavigationController()
+        nav.pushViewController(sut, animated: false)
+        nav.navigationBar.prefersLargeTitles = true
+
+        putInViewHierarchy(nav)
     }
 
     override func tearDown() {
+        sut = nil
         executeRunLoop()
         super.tearDown()
     }
 
+    var sutContainer: UIViewController { sut.navigationController! }
+
     func test_empty() {
-        let viewModel = EventsListViewModelFake()
-        let view = EventsListView()
-        let sut = EventsListController(
-            viewRoot: view,
-            viewModel: viewModel
-        )
-
-        putInViewHierarchy(sut)
-
-        FBSnapshotVerifyViewController(sut)
+        FBSnapshotVerifyViewController(sutContainer)
     }
 
     func test_empty_dark() {
-        let viewModel = EventsListViewModelFake()
-        let view = EventsListView()
-        let sut = EventsListController(
-            viewRoot: view,
-            viewModel: viewModel
-        )
+        sutContainer.view.window?.overrideUserInterfaceStyle = .dark
 
-        putInViewHierarchy(sut)
-
-        sut.viewRoot.window?.overrideUserInterfaceStyle = .dark
-
-        FBSnapshotVerifyViewController(sut)
+        FBSnapshotVerifyViewController(sutContainer)
     }
 }
