1bf8c62f9f9f716b4caa1b005b69890e9f06c4f7
StanislavMck2120 <stanislavmat@rozdoum.com>
2022-11-15 15:55:43 +0200

    Swiper action sending test, refactoring

--- Tests/ApplicationTests/SwiperTests.swift
+++ Tests/ApplicationTests/SwiperTests.swift
@@ -10,7 +10,10 @@ import XCTest
 
 class SwiperTests: XCTestCase {
     var sut: Swiper!
+    /// Created by `arrangeLittleSwipeRightInProgress()`
     var mockGestureRecognizer: UIPanGestureRecognizerMock?
+    /// Reference must be held for parent during pan gestures tests
+    private var parent: UIView!
 
     override func setUp() {
         super.setUp()
@@ -20,9 +23,11 @@ class SwiperTests: XCTestCase {
                                               height: EventCell.height))
         parentView.addSubview(sut)
         parentView.layoutIfNeeded()
+        parent = parentView
     }
 
     override func tearDown() {
+        parent = nil
         sut = nil
         super.tearDown()
     }
@@ -50,7 +55,7 @@ class SwiperTests: XCTestCase {
         XCTAssertLessThan(sut.initialX, sut.horizontalConstraint.constant)
     }
 
-    func test_swipingRight_notComplete_movesCircleToInitialPosition() {
+    func test_swipingRight_releaseAtMiddle_movesCircleToInitialPosition() {
         arrangeLittleSwipeRightInProgress()
 
         let exp = XCTestExpectation(description: "animation completed")
@@ -65,16 +70,35 @@ class SwiperTests: XCTestCase {
         XCTAssertEqual(sut.horizontalConstraint.constant, sut.initialX)
     }
 
-    private func arrangeLittleSwipeRightInProgress() {
+    func test_swipingRight_releaseAtEnd_sendsAction() {
+        let exp = XCTestExpectation(description: "action sent by swiper")
+        let spy = Spy(exp)
+        sut.addTarget(spy, action: #selector(Spy.handleAction), for: .primaryActionTriggered)
+
+        arrangeLittleSwipeRightInProgress(moveBy: .greatestFiniteMagnitude)
+        mockGestureRecognizer?.pan(location: nil, translation: nil, state: .ended)
+
+        wait(for: [exp], timeout: 0.1)
+    }
+
+    private func arrangeLittleSwipeRightInProgress(moveBy: CGFloat = 20) {
         let mockGR = UIPanGestureRecognizerMock(
             target: sut,
             action: #selector(Swiper.handlePan)
         )
-
         mockGestureRecognizer = mockGR
+
         sut.addGestureRecognizer(mockGR)
 
-        let someTranslation = CGPoint(x: 20, y: 0)
+        let someTranslation = CGPoint(x: moveBy, y: 0)
         mockGR.pan(location: nil, translation: someTranslation, state: .changed)
     }
+}
+
+private class Spy {
+    let exp: XCTestExpectation
+
+    init(_ exp: XCTestExpectation) { self.exp = exp }
+
+    @objc func handleAction() { exp.fulfill() }
 }
