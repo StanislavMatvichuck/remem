8e8bb80e5498593383d7a8c35ea625798c4c9367
StanislavMck2120 <stanislavmat@rozdoum.com>
2022-11-23 11:28:40 +0200

    DayDetailsViewController tests of cells rendering, refactoring

--- Tests/ApplicationTests/Controllers/DayControllerTests.swift
+++ Tests/ApplicationTests/Controllers/DayControllerTests.swift
@@ -15,14 +15,7 @@ class DayControllerTests: XCTestCase {
 
     override func setUp() {
         super.setUp()
-        let event = Event(name: "EventName")
-        let useCase = EventEditUseCasingFake()
-        let sut = DayController(date: .now, event: event, useCase: useCase)
-        sut.loadViewIfNeeded()
-
-        _ = UINavigationController(rootViewController: sut)
-
-        self.sut = sut
+        sut = makeSut()
     }
 
     override func tearDown() {
@@ -96,21 +89,62 @@ class DayControllerTests: XCTestCase {
         XCTAssertEqual(sut.viewRoot.happenings.numberOfRows(inSection: 0), 0)
     }
 
-    func test_oneHappening_showsFirstHappeningTime() throws {
+    func test_oneHappening_showsFirstHappeningTime() {
         let event = Event(name: "EventWithOneHappening")
         event.addHappening(date: .now)
 
-        let useCase = EventEditUseCasingFake()
-        sut = DayController(date: .now, event: event, useCase: useCase)
-        sut.loadViewIfNeeded()
+        sut = makeSut(event: event)
 
-        let table = sut.viewRoot.happenings
-        let firstCellIndex = IndexPath(row: 0, section: 0)
-        let cell = try XCTUnwrap(table.dataSource?.tableView(table, cellForRowAt: firstCellIndex) as? DayHappeningCell)
-
-        XCTAssertNotNil(cell.label.text?.firstIndex(of: ":"))
-        XCTAssertLessThanOrEqual(5, cell.label.text?.count ?? 0)
+        assertCellHasTimeText(at: sut.firstCellIndex)
     }
 
-    func test_manyHappenings_showsFewHappenings() {}
+    func test_manyHappenings_showsFewHappenings() {
+        let event = Event(name: "EventWithThreeHappenings")
+        event.addHappening(date: .now.addingTimeInterval(-1))
+        event.addHappening(date: .now.addingTimeInterval(-2))
+        event.addHappening(date: .now.addingTimeInterval(-3))
+
+        sut = makeSut(event: event)
+
+        let secondCellIndex = IndexPath(row: 1, section: 0)
+        let thirdCellIndex = IndexPath(row: 2, section: 0)
+
+        assertCellHasTimeText(at: sut.firstCellIndex)
+        assertCellHasTimeText(at: secondCellIndex)
+        assertCellHasTimeText(at: thirdCellIndex)
+    }
+
+    private func makeSut(event: Event = Event(name: "EventName")) -> DayController {
+        let useCase = EventEditUseCasingFake()
+        let sut = DayController(date: .now, event: event, useCase: useCase)
+        sut.loadViewIfNeeded()
+
+        _ = UINavigationController(rootViewController: sut)
+
+        return sut
+    }
+
+    private func assertCellHasTimeText(
+        at index: IndexPath,
+        file: StaticString = #file,
+        line: UInt = #line
+    ) {
+        let cell = sut.happening(at: index)
+        XCTAssertNotNil(cell?.label.text?.firstIndex(of: ":"), file: file, line: line)
+        XCTAssertLessThanOrEqual(5, cell?.label.text?.count ?? 0, file: file, line: line)
+    }
+}
+
+private extension DayController {
+    var table: UITableView { viewRoot.happenings }
+    var firstCellIndex: IndexPath { IndexPath(row: 0, section: 0) }
+
+    func happening(at index: IndexPath) -> DayHappeningCell? {
+        do {
+            let cell = try XCTUnwrap(table.dataSource?.tableView(table, cellForRowAt: index) as? DayHappeningCell)
+            return cell
+        } catch {
+            fatalError("happening getting error")
+        }
+    }
 }
