1c5fe543eba913d8d989fcc1468a2d009d9184cd
StanislavMck2120 <stanislavmat@rozdoum.com>
2022-11-23 10:54:20 +0200

    DayController integration tests added

--- Tests/ApplicationTests/Controllers/DayControllerTests.swift
+++ Tests/ApplicationTests/Controllers/DayControllerTests.swift
@@ -7,6 +7,7 @@
 
 @testable import Application
 import Domain
+import ViewControllerPresentationSpy
 import XCTest
 
 class DayControllerTests: XCTestCase {
@@ -17,6 +18,10 @@ class DayControllerTests: XCTestCase {
         let event = Event(name: "EventName")
         let useCase = EventEditUseCasingFake()
         let sut = DayController(date: .now, event: event, useCase: useCase)
+        sut.loadViewIfNeeded()
+
+        _ = UINavigationController(rootViewController: sut)
+
         self.sut = sut
     }
 
@@ -25,7 +30,65 @@ class DayControllerTests: XCTestCase {
         super.tearDown()
     }
 
-    func test_empty() {
-        XCTAssertLessThan(3, sut.title?.count ?? 0)
+    func test_hasTitle() {
+        let numberString = sut.title!.split(separator: " ").first
+        let wordString = sut.title!.split(separator: " ").last
+
+        XCTAssertNotNil(Int(numberString!))
+        XCTAssertLessThanOrEqual(3, wordString!.count)
     }
+
+    func test_hasEditButton() {
+        let editButton = sut.navigationItem.rightBarButtonItem
+
+        XCTAssertEqual(editButton?.title, String(localizationId: "button.edit"))
+    }
+
+    func test_editButtonTap_enablesEditMode() {
+        let editButton = sut.navigationItem.rightBarButtonItem
+
+        XCTAssertFalse(sut.viewRoot.happenings.isEditing, "precondition")
+
+        tap(editButton!)
+
+        XCTAssertTrue(sut.viewRoot.happenings.isEditing)
+    }
+
+    func test_editButtonTap_tapAgain_disablesEditMode() {
+        let editButton = sut.navigationItem.rightBarButtonItem
+
+        XCTAssertFalse(sut.viewRoot.happenings.isEditing, "precondition")
+
+        tap(editButton!)
+        tap(editButton!)
+
+        XCTAssertFalse(sut.viewRoot.happenings.isEditing)
+    }
+
+    func test_hasCreateButton() {
+        let createButton = sut.navigationItem.leftBarButtonItem
+
+        XCTAssertEqual(createButton?.title, String(localizationId: "button.create"))
+    }
+
+    func test_createButtonTap_presentsAlert() {
+        let verifier = AlertVerifier()
+        let createButton = sut.navigationItem.leftBarButtonItem
+
+        tap(createButton!)
+
+        verifier.verify(
+            title: String(localizationId: "button.create"),
+            message: "",
+            animated: true,
+            actions: [
+                .cancel(String(localizationId: "button.cancel")),
+                .default(String(localizationId: "button.create"))
+            ]
+        )
+    }
+
+    func test_empty_showsNothing() {}
+    func test_oneHappening_showsFirstHappeningTime() {}
+    func test_manyHappenings_showsFewHappenings() {}
 }
