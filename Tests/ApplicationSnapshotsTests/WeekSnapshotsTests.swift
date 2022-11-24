cb29c1520ec7b5e73a8c3d8df24ff08a682ea0a8
StanislavMck2120 <stanislavmat@rozdoum.com>
2022-11-24 18:49:44 +0200

    WeekSnapshotsTests added

---
+++ Tests/ApplicationSnapshotsTests/WeekSnapshotsTests.swift
@@ -0,0 +1,39 @@
+//
+//  WeekSnapshotsTests.swift
+//  ApplicationSnapshotsTests
+//
+//  Created by Stanislav Matvichuck on 24.11.2022.
+//
+
+@testable import Application
+import Domain
+import iOSSnapshotTestCase
+
+class WeekSnapshotsTest: FBSnapshotTestCase {
+    var sut: WeekController!
+    
+    override func setUp() {
+        super.setUp()
+        recordMode = true
+        let coordinator = ApplicationFactory().makeCoordinator()
+        let useCase = EventEditUseCasingFake()
+        let event = Event(name: "Event")
+        sut = WeekController(
+            event: event,
+            useCase: useCase,
+            coordinator: coordinator
+        )
+        
+        putInViewHierarchy(sut)
+    }
+    
+    override func tearDown() {
+        executeRunLoop()
+        sut = nil
+        super.tearDown()
+    }
+    
+    func test_empty() {
+        FBSnapshotVerifyViewController(sut)
+    }
+}
