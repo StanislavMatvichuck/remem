3f6cecc9ec14981ec5fc9a2f1e76ac48daef26f8
StanislavMck2120 <stanislavmat@rozdoum.com>
2023-01-31 13:38:30 +0200

    ui tests added, performance removed

---
+++ ApplicationUITests/ApplicationUITests.swift
@@ -0,0 +1,28 @@
+//
+//  ApplicationUITests.swift
+//  ApplicationUITests
+//
+//  Created by Stanislav Matvichuck on 31.01.2023.
+//
+
+import XCTest
+
+final class ApplicationUITests: XCTestCase {
+//    func testLaunchPerformance() throws {
+//        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
+//            // This measures how long it takes to launch your application.
+//            measure(metrics: [XCTApplicationLaunchMetric()]) {
+//                XCUIApplication().launch()
+//            }
+//        }
+//    }
+
+    func test_addEvent() {
+        measure {
+            let app = XCUIApplication()
+            app.launch()
+            app.buttons["Create event"].tap()
+            app.buttons["Cancel"].tap()
+        }
+    }
+}
