5ad2bab43b7708d7a3f53116cd24a5eed34ffd76
StanislavMck2120 <stanislavmat@rozdoum.com>
2023-02-24 14:58:46 +0200

    added DayChangeWatcher type with tests

---
+++ Application/Helpers/DayChangeWatcher.swift
@@ -0,0 +1,10 @@
+//
+//  DayChangeWatcher.swift
+//  Application
+//
+//  Created by Stanislav Matvichuck on 24.02.2023.
+//
+
+import Foundation
+
+struct DayChangeWatcher {}
---
+++ Tests/ApplicationTests/DayChangeWatcherTests.swift
@@ -0,0 +1,15 @@
+//
+//  DayChangeWatcherTests.swift
+//  ApplicationTests
+//
+//  Created by Stanislav Matvichuck on 24.02.2023.
+//
+
+@testable import Application
+import XCTest
+
+final class DayChangeWatcherTests: XCTestCase {
+    func test_init() {
+        let sut = DayChangeWatcher()
+    }
+}
