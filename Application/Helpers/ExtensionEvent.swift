3769db364a240d629665920313032a84debc7aba
StanislavMck2120 <stanislavmat@rozdoum.com>
2023-04-22 15:31:29 +0300

    WeekDay file

--- Application/Helpers/ExtensionEvent.swift
+++ Application/Helpers/ExtensionEvent.swift
@@ -8,20 +8,6 @@
 import Domain
 import Foundation
 
-public enum WeekDay: Int, CaseIterable {
-    case monday = 2
-    case tuesday = 3
-    case wednesday = 4
-    case thursday = 5
-    case friday = 6
-    case saturday = 7
-    case sunday = 1
-
-    public static func make(_ date: Date) -> WeekDay {
-        WeekDay(rawValue: Calendar.current.dateComponents([.weekday], from: date).weekday!)!
-    }
-}
-
 extension Event {
     func happenings(forDayIndex day: DayIndex) -> [Happening] {
         let startOfDay = Calendar.current.startOfDay(for: day.date)
---
+++ Application/Helpers/Timelines/WeekDay.swift
@@ -0,0 +1,22 @@
+//
+//  WeekDay.swift
+//  Domain
+//
+//  Created by Stanislav Matvichuck on 21.04.2023.
+//
+
+import Foundation
+
+public enum WeekDay: Int, CaseIterable {
+    case monday = 2
+    case tuesday = 3
+    case wednesday = 4
+    case thursday = 5
+    case friday = 6
+    case saturday = 7
+    case sunday = 1
+
+    public static func make(_ date: Date) -> WeekDay {
+        WeekDay(rawValue: Calendar.current.dateComponents([.weekday], from: date).weekday!)!
+    }
+}
