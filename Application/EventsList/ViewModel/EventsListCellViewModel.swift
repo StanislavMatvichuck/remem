6c854fcae1620b970b1ec2f57ca35bcfeddd7de0
StanislavMck2120 <stanislavmat@rozdoum.com>
2024-03-30 00:46:05 +0200

    cleanup

--- Application/EventsList/ViewModel/EventsListCellViewModel.swift
+++ Application/EventsList/ViewModel/EventsListCellViewModel.swift
@@ -7,54 +7,8 @@
 
 import Foundation
 
-protocol EventsListCellViewModel: Identifiable, Equatable {
-    var id: String { get }
-    func requiresUpdate(oldValue: any EventsListCellViewModel) -> Bool
-}
+protocol EventsListCellViewModel {}
 
-extension HintCellViewModel: EventsListCellViewModel {
-    var id: String { "HintCellViewModel" }
-
-    static func == (lhs: HintCellViewModel, rhs: HintCellViewModel) -> Bool {
-        lhs.title == rhs.title &&
-            lhs.highlighted == rhs.highlighted
-    }
-
-    func requiresUpdate(oldValue: any EventsListCellViewModel) -> Bool {
-        id == oldValue.id && self != oldValue as? HintCellViewModel
-    }
-}
-
-extension EventCellViewModel: EventsListCellViewModel {
-    /// id: String uses internal property
-
-    static func == (lhs: EventCellViewModel, rhs: EventCellViewModel) -> Bool {
-        lhs.title == rhs.title &&
-            lhs.value == rhs.value &&
-            lhs.timeSince == rhs.timeSince &&
-            lhs.hintEnabled == rhs.hintEnabled &&
-            lhs.progress == rhs.progress &&
-            lhs.progressState == rhs.progressState &&
-            lhs.goalAmount == rhs.goalAmount &&
-            lhs.animation == rhs.animation
-    }
-
-    func requiresUpdate(oldValue: any EventsListCellViewModel) -> Bool {
-        let identifiersCondition = id == oldValue.id
-        let equalityCondition = self != oldValue as? EventCellViewModel
-        let animationCondition = animation != .none
-        return identifiersCondition && (equalityCondition || animationCondition)
-    }
-}
-
-extension CreateEventCellViewModel: EventsListCellViewModel, Equatable {
-    var id: String { "CreateEventCellViewModel" }
-
-    static func == (lhs: CreateEventCellViewModel, rhs: CreateEventCellViewModel) -> Bool {
-        lhs.isHighlighted == rhs.isHighlighted
-    }
-
-    func requiresUpdate(oldValue: any EventsListCellViewModel) -> Bool {
-        id == oldValue.id && self != oldValue as? CreateEventCellViewModel
-    }
-}
+extension HintCellViewModel: EventsListCellViewModel {}
+extension EventCellViewModel: EventsListCellViewModel {}
+extension CreateEventCellViewModel: EventsListCellViewModel {}
